38fe05193d49104fdaa402fc1f4a0e0ab465d2cc
Josh Cheek <josh.cheek@gmail.com>
2017-07-24 09:20:20 -0500

    Uhm, improooovements, maybe?

--- markov.rb
+++ markov.rb
@@ -17,11 +17,16 @@ end
 
 chain = Hash.new { |h, k| h[k] = [] }
 start_words = []
+matchers = []
+while ARGV[0] =~ /--match=/
+  ARGV.shift
+  matchers << $'
+end
 
 # http://www.abrahamlincolnonline.org/
 # http://www.hitler.org/writings/Mein_Kampf/
 ARGV.each do |file|
-  tokens = File.read(file).split(/\s+|(?<!-)\b(?!-)/).map(&:downcase)
+  tokens = File.read(file).split(/\s+|(?<![-'])\b(?![-'])/).map(&:downcase)
   start_words << tokens.take(2)
   tokens.each_cons 3 do |pre1, pre2, post|
     chain[[pre1, pre2]] << post
@@ -29,12 +34,17 @@ ARGV.each do |file|
   end
 end
 
-
-sentence = []
-word1, word2 = start_words.sample
-while word1
-  sentence << word1
-  break sentence << word2 if end_of_sentence? word2
-  word1, word2 = word2, chain[[word1, word2]].sample
+loop do
+  words = []
+  word1, word2 = start_words.sample
+  while word1
+    words << word1
+    break words << word2 if end_of_sentence? word2
+    word1, word2 = word2, chain[[word1, word2]].sample
+  end
+  sentence = join_sentence words
+  if matchers.all? { |m| sentence.downcase.include? m.downcase }
+    puts sentence
+    break
+  end
 end
-puts join_sentence sentence
