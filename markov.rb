754010db6ce938f3b6708d97499acbe90f5796c5
Josh Cheek <josh.cheek@gmail.com>
2017-07-23 21:41:12 -0500

    Every 2 words + more Lincoln

--- markov.rb
+++ markov.rb
@@ -2,7 +2,7 @@ def join_sentence(sentence)
   return '' if sentence.empty?
   joined = sentence[0].capitalize
   sentence.each_cons 2 do |pre, post|
-    if post =~ /^\W$/
+    if post =~ /^[;\.,]$/
       joined << post
     else
       joined << " " << post
@@ -11,27 +11,30 @@ def join_sentence(sentence)
   joined
 end
 
+def end_of_sentence?(word)
+  word == '.' || word == '!' || word == '?'
+end
+
 chain = Hash.new { |h, k| h[k] = [] }
 start_words = []
 
 # http://www.abrahamlincolnonline.org/
 # http://www.hitler.org/writings/Mein_Kampf/
-
 ARGV.each do |file|
-  tokens = File.read(file).split(/\s+|\b/).map(&:downcase)
-  start_words << tokens[0].downcase
-  tokens.each_cons 2 do |pre, post|
-    chain[pre] << post
-    start_words << post if pre == '.'
+  tokens = File.read(file).split(/\s+|(?<!-)\b(?!-)/).map(&:downcase)
+  start_words << tokens.take(2)
+  tokens.each_cons 3 do |pre1, pre2, post|
+    chain[[pre1, pre2]] << post
+    start_words << [pre2, post] if end_of_sentence? pre1
   end
 end
 
 
 sentence = []
-crnt = start_words.sample
-while crnt
-  sentence << crnt
-  break if crnt == '.'
-  crnt = chain[crnt].sample
+word1, word2 = start_words.sample
+while word1
+  sentence << word1
+  break sentence << word2 if end_of_sentence? word2
+  word1, word2 = word2, chain[[word1, word2]].sample
 end
 puts join_sentence sentence
